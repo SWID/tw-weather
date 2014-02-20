4b9b15bebca34d72482cb0a5e7675b8306102c39
Dca <dca35311@gmail.com>
2014-02-20 13:34:06 +0800

    Add api

--- test/main.js
+++ test/main.js
@@ -1,13 +1,34 @@
 var assert = require("assert");
 
-var Weather = require('../');
+var weather = require('../');
 
 describe('tw.weather', function () {
     describe('init', function () {
 
         // init
         it('should have a weather object', function (done) {
-            if (typeof Weather !== 'object') throw new Error('init failed');
+            if (typeof weather !== 'object') throw new Error('typeof weather is ' + typeof weather);
+            done();
+        });
+
+        it('should have a api `assistant` ', function (done) {
+            if (typeof weather.assistant !== 'function') {
+                throw new Error('type is ' + typeof weather.assistant);
+            }
+            done();
+        });
+
+        it('should have a api `forecast` ', function (done) {
+            if (typeof weather.forecast !== 'function') {
+                throw new Error('type is ' + typeof weather.forecast);
+            }
+            done();
+        });
+
+        it('should have a api `week` ', function (done) {
+            if (typeof weather.week !== 'function') {
+                throw new Error('type is ' + typeof weather.week);
+            }
             done();
         });
 
